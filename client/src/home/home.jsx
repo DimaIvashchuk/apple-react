e8d21ff40bb39a7f226d1db37ba870367b68685c
Denis Maluovany <mineden2300mo@gmail.com>
2020-08-02 18:28:36 +0300

    Added sidebar and header template, edited index.html, index.css and home.jsx

--- client/src/home/home.jsx
+++ client/src/home/home.jsx
@@ -1,13 +1,18 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../utils/authSetup/authSetup.context'
+import { Header } from './header/header'
+import { Sidebar } from './sidebar/sidebar'
+
+import './home.scss'
+
 
 export const Home = () => {
-
     const auth = useContext(AuthContext)
 
     return(
-        <div>
-            <button onClick={auth.logout}>logout</button>
+        <div className="home">
+            <Sidebar />
+            <Header />
         </div>
     )
 }
